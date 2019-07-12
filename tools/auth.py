fe7b0267da3e94b9f62164fef33463558381d3ce
breaklinux <13164278484@163.com>
2019-07-12 16:40:38 +0800

    Create auth.py
    认证装饰器

---
+++ tools/auth.py
@@ -0,0 +1,21 @@
+from flask import request,Response
+from models import channel,ipwhilt
+def Check(fun):
+    def checkAuth(*args, **kwargs):
+        import json
+        if request.method != "GET":
+            Data = request.get_json()
+            uuid = Data.get("channelID", None)
+            realIp=request.headers['X-Real-IP']
+            UUID = channel.query.filter_by(uuid=uuid).all()
+            Ipwhilt=ipwhilt.query.filter_by(ip=realIp).all()
+            if UUID and Ipwhilt:
+                return fun(*args, **kwargs)
+            elif not UUID:
+                data = {"code": 403, "message": "channelID No Find"}
+            else:
+                data = {"code": 403, "message": "ip {realIp} 没权限,请申请授权".format(realIp=realIp)}
+        else:
+            return fun(*args, **kwargs)
+        return Response(json.dumps(data), mimetype='application/json')
+    return checkAuth
